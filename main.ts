043da228a3f616a33e468ecbd1d5bc39885b762b
Matheus Kuster <matheuskuster@hotmail.com>
2023-10-20 10:17:22 -0300

    feat: make swagger docs

--- main.ts
+++ main.ts
@@ -1,7 +1,13 @@
 import 'dotenv/config';
 
-import { ConsoleLogger, ValidationPipe, VersioningType } from '@nestjs/common';
+import {
+  ConsoleLogger,
+  INestApplication,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './src/infra/app.module';
 
@@ -19,10 +25,26 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  setupSwagger(app);
+
   const port = process.env.SERVER_PORT ?? 3000;
   await app.listen(port);
 
   logger.log(`🚀 Server started successfully on port ${port}`);
+}
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Inova Week Management')
+    .setDescription(
+      'API para gerenciamento de edições, projetos, usuários e avaliações do InovaWeek',
+    )
+    .setVersion('1.0')
+    .addTag('inovaweek')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 }
 
 bootstrap();
